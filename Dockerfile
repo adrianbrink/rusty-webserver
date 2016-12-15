9304dccfd2add3fb5ddb5dda8b9e730b945b9634
Adrian Brink <adrian@brink-holdings.com>
2016-12-15 21:58:23 +0100

    Giving up on dockerizing this for now

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN apt-get update && \
     apt-get install -y sqlite libsqlite3-dev
 RUN apt-get install -y libpq-dev
 RUN cargo install diesel_cli
-EXPOSE 40000
+EXPOSE 4443
 VOLUME ["/source"]
 WORKDIR /source
 CMD ["/bin/bash"]
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use nickel::{Nickel, HttpRouter};
 use postgres::{Connection, TlsMode};
 
 fn main() {
+    // let client = twilio::Client::new("AC48b3955fdf390d1ee7fe52efcabbfb2d", "dcd49ce427035532f62585eeb7c63af0");
     let mut server = Nickel::new();
     server.get("**", middleware!("Hello World"));
-    server.listen("127.0.0.1:40000");
+    server.listen("127.0.0.1:4443");
     println!("Hello, world!");
     // let _server = Iron::new(hello_world).http("localhost:4040").expect("Server didn't start.");
     // let postgres_ip = env::var("POSTGRES_PORT_5432_TCP_ADDR")
