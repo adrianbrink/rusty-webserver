8b2c07a8d6a5f895bdbc169f9ac25832f9f74618
Adrian Brink <adrian@brink-holdings.com>
2016-12-15 21:00:39 +0100

    Update the server ips

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM adrianbrink/rust:nightly
 
+RUN apt-get update && \
+    apt-get install -y sqlite libsqlite3-dev
+RUN apt-get install -y libpq-dev
+RUN cargo install diesel_cli
+EXPOSE 40000
 VOLUME ["/source"]
 WORKDIR /source
 CMD ["/bin/bash"]
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 extern crate postgres;
+extern crate iron;
+#[macro_use]
+extern crate nickel;
 
+use iron::prelude::*;
+use iron::status;
 use std::env;
+use nickel::{Nickel, HttpRouter};
 use postgres::{Connection, TlsMode};
 
 fn main() {
+    let mut server = Nickel::new();
+    server.get("**", middleware!("Hello World"));
+    server.listen("127.0.0.1:40000");
     println!("Hello, world!");
-    let postgres_ip = env::var("POSTGRES_PORT_5432_TCP_ADDR")
-        .expect("This container needs to be linked with a container running postgres.");
-    let postgres_connection_string = format!("postgres://postgres@{}", postgres_ip);
-    let conn = Connection::connect(&*postgres_connection_string, TlsMode::None)
-        .expect("This might work");
+    // let _server = Iron::new(hello_world).http("localhost:4040").expect("Server didn't start.");
+    // let postgres_ip = env::var("POSTGRES_PORT_5432_TCP_ADDR")
+    //     .expect("This container needs to be linked with a container running postgres.");
+    // let postgres_connection_string = format!("postgres://postgres@{}", postgres_ip);
+    // let _conn = Connection::connect(&*postgres_connection_string, TlsMode::None)
+    //     .expect("This might work");
+
+    // Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello World!"))))
+    //     .http("localhost:4040")
+    //     .unwrap();
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    println!("dddd");
+    Ok(Response::with((status::Ok, "Hello World!")))
 }
