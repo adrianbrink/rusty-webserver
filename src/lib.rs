21177df76134940090ec7f4e0963344658151950
Adrian Brink <adrian@brink-holdings.com>
2016-12-15 23:54:21 +0100

    Start implementing

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,30 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate sendgrid;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+use sendgrid::v3::V3Sender;
+
+pub fn establish_postgres_connection() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub struct Mailer {
+    sender: V3Sender,
+}
+
+impl Mailer {
+    pub fn new(api_key: String) -> Mailer {
+        let sender = V3Sender::new(api_key);
+        Mailer { sender: sender }
+    }
+}
+
 #[test]
 fn it_works() {}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
 extern crate postgres;
-extern crate iron;
+extern crate webserver_rust;
 #[macro_use]
 extern crate nickel;
 
-use iron::prelude::*;
-use iron::status;
 use std::env;
+use webserver_rust::*;
 use nickel::{Nickel, HttpRouter};
 use postgres::{Connection, TlsMode};
 
 fn main() {
-    // let client = twilio::Client::new("AC48b3955fdf390d1ee7fe52efcabbfb2d", "dcd49ce427035532f62585eeb7c63af0");
     let mut server = Nickel::new();
     server.get("**", middleware!("Hello World"));
-    server.listen("127.0.0.1:4443");
+    server.listen("127.0.0.1:8080");
     println!("Hello, world!");
-    // let _server = Iron::new(hello_world).http("localhost:4040").expect("Server didn't start.");
+
     // let postgres_ip = env::var("POSTGRES_PORT_5432_TCP_ADDR")
     //     .expect("This container needs to be linked with a container running postgres.");
     // let postgres_connection_string = format!("postgres://postgres@{}", postgres_ip);
     // let _conn = Connection::connect(&*postgres_connection_string, TlsMode::None)
     //     .expect("This might work");
-
-    // Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello World!"))))
-    //     .http("localhost:4040")
-    //     .unwrap();
-}
-
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    println!("dddd");
-    Ok(Response::with((status::Ok, "Hello World!")))
 }
